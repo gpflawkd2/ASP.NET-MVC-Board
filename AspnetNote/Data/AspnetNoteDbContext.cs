31456b5aeaac5cd0fae8725d9cb61b4c82b44d2e
gpflawkd2 <gpflawkd2@gmail.com>
2021-03-05 11:03:04 +0900

    EF Core Migration Database Schema

---
+++ AspnetNote/Data/AspnetNoteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspnetNote.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetNote.Data
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=AspnetNoteDb;User Id=Board;Password=phr8611!;");
+        }
+    }
+}
