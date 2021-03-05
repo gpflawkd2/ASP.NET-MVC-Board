aa13a9b42ac6183e2ff12d63fe0dc09f8eb3066d
gpflawkd2 <gpflawkd2@gmail.com>
2021-03-05 14:37:33 +0900

    Add Join Mebership

---
+++ AspnetNote/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetNote.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 회원가입
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if(ModelState.IsValid)
+            {
+                // DB Connection Open
+                using (var db = new Data.AspnetNoteDbContext())
+                {
+                    db.Users.Add(model);
+                    db.SaveChanges();   //Commit
+
+                }
+                // DB Connection Close
+
+                return RedirectToAction("Index","Home");
+
+            }
+            return View();
+        }
+    }
+}
