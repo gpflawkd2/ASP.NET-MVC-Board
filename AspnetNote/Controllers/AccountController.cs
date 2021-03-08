518d5cb982ae9cb965767e1f38e6f3bb9aa5d7db
gpflawkd2 <gpflawkd2@gmail.com>
2021-03-08 14:08:57 +0900

    Add Login Logic

--- AspnetNote/Controllers/AccountController.cs
+++ AspnetNote/Controllers/AccountController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using AspnetNote.Models;
+using AspnetNote.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,6 +18,36 @@ namespace AspnetNote.Controllers
             return View();
         }
 
+        [HttpPost] 
+        public IActionResult Login(LoginViewModel model)
+        {
+            // ID, 비밀번호 - 필수
+            if(ModelState.IsValid)
+            {
+                using (var db = new Data.AspnetNoteDbContext())
+                {
+                    // Linq - Method Chaining
+                    // FirstOrDefault() : 조건을 만족하는 첫번째 데이터 출력
+                    // => : A Got to B
+                    // var user = db.Users.FirstOrDefault(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
+                    var user = db.Users
+                        .FirstOrDefault(u => u.UserId.Equals(model.UserId) &&
+                                        u.UserPassword.Equals(model.UserPassword));
+
+                    if (user != null)
+                    {
+                        // 로그인에 성공했을 때
+                        return RedirectToAction("LoginSuccess", "Home");
+                    }
+                }
+
+                // 로그인에 실패했을 때
+                ModelState.AddModelError(string.Empty, "사용자 ID 혹은 비밀번호가 올바르지 않습니다.");
+            }
+
+            return View(model);
+        }
+
         /// <summary>
         /// 회원가입
         /// </summary>
@@ -29,6 +57,11 @@ namespace AspnetNote.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 회원가입 전송
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Register(User model)
         {
